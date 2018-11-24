b2864cfad6d2d919092c241129aa36f2d1583c3d
bosiddon <beau.tadej@outlook.com>
2018-11-24 11:33:21 -0800

    Revert "Removed http2 package"
    This reverts commit 342577608962130a70f01bf649a4bdc12e57da83.

--- index.js
+++ index.js
@@ -284,20 +284,20 @@ function StartTwitterStream( options ) {
 
 		twitterClient.on( 'error', function ( error ) {
 			errors.push( { date: new Date().toDateString(), time: new Date().toTimeString(), message: "Twitter Client Error", data: error } );
-			console.log( "Twitter Client Error", JSON.stringify( error ) );
+			console.log( "Twitter Client Error", error.stack );
 			twitterClient.abort();
 		} );
 
 		twitterClient.on( 'reconnect', function ( reconnect ) {
 			errors.push( { date: new Date().toDateString(), time: new Date().toTimeString(), message: "Twitter Client Reconnect", data: reconnect } );
-			console.log( "Twitter Client Reconnect", JSON.stringify( reconnect ) );
+			console.log( "Twitter Client Reconnect", reconnect.stack );
 			twitterClient.reconnect();
 		} );
 
 		twitterClient.track( keywords );
 	} catch ( error ) {
 		errors.push( { date: new Date().toDateString(), time: new Date().toTimeString(), message: "Twitter Client Creation Error", data: error } );
-		console.log( "Twitter Client Error", JSON.stringify( error ) );
+		console.log( "Twitter Client Error", error.stack );
 		twitterClient.abort();
 	}
 }
